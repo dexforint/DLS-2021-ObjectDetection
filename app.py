e8026d221d2168f96bd5fe259aac0af304a57758
dexforint <dexforint@mail.ru>
2021-06-28 00:03:42 +0300

    Update app.py

--- app.py
+++ app.py
@@ -187,5 +187,5 @@ def page404(request, response):
     response.status = 301
     response.location = f"{SITE_ADDRESS}/"
 
-run_simple('localhost', 80, app, use_reloader=True,
+run_simple('0.0.0.0', 80, app, use_reloader=True,
            reloader_type="watchdog")  # , use_debugger=True
